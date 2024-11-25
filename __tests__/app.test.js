cee144ea16ce1d84f70fe2b8a827e46fc1862baa
Ghiffari <ghiffariaulia90@gmail.com>
2024-11-25 18:52:46 +0000

    Added task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -52,3 +52,43 @@ describe("GET non-existend URL", () => {
       });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an article object with the article_id from the URL", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body: { article }  }) => {
+        expect(article).toEqual({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          created_at: new Date(1604394720000).toISOString(),
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      })
+  });
+
+  test("400: Responds with a message 'Invalid id type' if the article_id in the URL is invalid", () => {
+    return request(app)
+      .get('/api/articles/one')
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe('Invalid id type')
+      });
+  });
+
+  test("404: Responds with a message 'not found' if the article_id in the URL is non-existent", () => {
+    return request(app)
+      .get('/api/articles/100')
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        console.log(msg, "<<<--- msg");
+        expect(msg).toBe('not found');
+      });
+  });
+});
